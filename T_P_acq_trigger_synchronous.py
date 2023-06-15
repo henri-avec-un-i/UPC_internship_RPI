4194805ba206054920a9ddd4f8e7590d03650223
henri-fabre <133638361+henri-avec-un-i@users.noreply.github.com>
2023-06-15 16:00:06 +0200

    Update T_P_acq_trigger_synchronous.py

--- T_P_acq_trigger_synchronous.py
+++ T_P_acq_trigger_synchronous.py
@@ -6,13 +6,42 @@ from __future__ import print_function #Must be at the beginning of the file for 
 
 Description:
 
-TD : Wrap the whole script in a function, document well, add error handling ?
+This script performs data acquisition and logging of temperature and pressure measurements triggered by an external input signal. It initializes the hardware components, sets up the GPIO pins, and defines the data structures and constants used in the script. The main logic of the script includes a callback function that is executed when the trigger input is detected, updating the data array with a new line for each trigger event. The script continuously displays temperature and pressure data on an LCD screen while waiting for trigger events. If a keyboard interrupt signal is received, the data array is saved as a CSV file, and the script terminates by cleaning up the GPIO pins.
 
-TO IMPLEMENT LATER WITH THREADING
-#Time after which the acquisition stops and the data is saved as a CSV, in seconds
-timeout_delay = 5
+Note: The code contains placeholders for implementing threading and a timeout delay for data acquisition, which can be implemented later.
 
+Imported Libraries:
+- RPi.GPIO: Library for controlling GPIO pins on Raspberry Pi.
+- time: Library for time-related functions.
+- csv: Library for CSV file handling.
+- sys: Library for system-specific parameters and functions.
+- numpy: Library for array manipulation.
+- RPLCD: Library for controlling LCD displays.
+- T_P_acq_func: Custom library for temperature and pressure acquisition functions.
+- daqhats: Library for interacting with MCC DAQ HATs.
+- daqhats_utils: Library for utility functions related to MCC DAQ HATs.
+
+Hardware Initialization:
+- MCC HATS: Initialization of MCC 128 and MCC 134 DAQ HATs, setting input modes and ranges.
+- GPIO Pins: Setting up the GPIO pins on Raspberry Pi for trigger input.
+- LCD Setup: Initializing the LCD object with pin configurations.
+
+Data Structure Initialization:
+- data_array: Initializing an empty array to store temperature and pressure measurements.
+- header: A list containing column names for the data array.
+- filename: Name of the CSV file to save the data.
+- T_hot_wall: Hot wall temperature in Celsius.
+- pressure_alarm: Threshold value for pressure alarm and system shutdown.
+- rising_edge_counter: Counter for the number of trigger events.
+
+Main Script Logic:
+- data_array_update: Callback function executed when the trigger input is detected. It updates the data array with a new line containing the index, relative time, and current temperature and pressure measurements.
+- GPIO event detection: Adding event detection for the rising edge of the trigger input, calling the data_array_update function.
+- Main loop: Continuously displays temperature and pressure data on the LCD screen while waiting for trigger events.
+- KeyboardInterrupt handling: If a keyboard interrupt signal is received, the data array is saved as a CSV file, and the script exits after cleaning up the GPIO pins.
 """
+
+
 ################################################
 
 """
@@ -97,8 +126,8 @@ filename = 'data_single_read_trigger.csv'
 data_array = []
 
 ### Definition of script constants
-T_hot_wall = 50 # Hot wall temperature, to specify, in Celcius, to measure later directly on the Peltier module
-pressure_alarm = 130 #Pressure alarm threshold for alarm and system shutdown
+T_hot_wall = 50 # Hot wall temperature in Celcius, to specify, in Celcius, to measure later directly on the Peltier module
+pressure_alarm = 130 #Pressure alarm threshold in bar for alarm and system shutdown
 
 #Initialisation of rising_edge_counter, ie. number of trigger signal received, indicate the index of the current measure
 rising_edge_counter = 0
@@ -111,9 +140,10 @@ Main script logic
 """
 
 #Function executed at each trigger event
-def trigger_callback(trigger_pin):
+def data_array_update(trigger_pin):
     """
-    Callback function to be executed when the trigger input is detected.
+    Callback function to be executed when the trigger input is detected. Updates the data array with a new line each time it is called. 
+    
 
     Args:
         channel (int): The GPIO channel number that triggered the callback.
@@ -142,17 +172,13 @@ def trigger_callback(trigger_pin):
     
     
     rising_edge_counter += 1
-    
-    #print(rising_edge_counter)
 
-
-# Add the event detection for the falling edge of the trigger input
-GPIO.add_event_detect(trigger_pin, GPIO.RISING, callback=trigger_callback)
+# Add the event detection for the rising edge of the trigger input
+GPIO.add_event_detect(trigger_pin, GPIO.RISING, callback=data_array_update)
 
 
 try:
-    #While no trigger event, just display the pressure and temperature data, need to update function
-    #in order to use LCD display
+    #While no trigger event, just display the pressure and temperature data
     while True:
         #Function that display T and P data continuoulsy, Pressure alarm in bar
         #Need to pay attention to the refresh rate COMPARED TO acquisition rate
